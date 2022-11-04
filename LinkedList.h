aff835b0c9599340f02f9d3b3813695464530af2
fearaizor <noluckanytime@gmail.com>
2022-11-05 00:16:22 +0700

    added 2 ctor, size method and print method

--- LinkedList.h
+++ LinkedList.h
@@ -1,6 +1,9 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+#include <iostream>
+#include <initializer_list>
+
 enum Mode{
     Circular,
     Singly,
@@ -31,9 +34,56 @@ template<class type> class LinkedList<type, Singly>{
         using Node = _Singly<type>;
         Node *head;
         Node *tail;
+        int length;
 
     public:
-        
+        LinkedList(){
+            head = tail = nullptr;
+            length = 0;
+        }
+
+        LinkedList(std::initializer_list<type> data){
+            head = tail = nullptr;
+            length = 0;
+            typename std::initializer_list<type>::iterator it = data.begin();
+
+            while(it != data.end()){
+                this->addBack(*it);
+                it++;
+            }
+        }
+
+        void addBack(type data){
+            Node *temp = new Node;
+            temp->data = data;
+            temp->next = nullptr;
+
+            if(head == nullptr){
+                head = tail = temp;
+                length = 1;
+            }
+            else{
+                tail->next = temp;
+                tail = temp;
+                length++;
+            }
+        }
+
+        int size(){
+            return length;
+        }
+
+        void print(){
+            Node *temp = head;
+
+            while(temp != nullptr){
+                std::cout<<temp->data<<" ";
+                temp = temp->next;
+            }
+
+            std::cout<<"\n"<<length<<"\n";
+        }
+
 };
 
 #endif
